d4d8ef887d17e1648a31bf6eea2260b7540cfbe4
Jonathan Borges <jonathan@dev2world.com>
2017-01-31 15:17:15 -0200

    livereload

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,7 @@ var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
@@ -14,7 +15,10 @@ module.exports = webpackMerge(commonConfig, {
   },
 
   plugins: [
-    new ExtractTextPlugin('[name].css')
+    new ExtractTextPlugin('[name].css'),
+    new LiveReloadPlugin({
+      appendScriptTag: true
+    })
   ],
 
   devServer: {
